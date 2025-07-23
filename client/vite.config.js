7d935f482bc52f0edb0640d4fcdd83c10d40faef
Van Nguyen <vdnnguyen94@gmail.com>
2025-07-23 10:04:45 -0400

    update history fallback client

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import history from 'connect-history-api-fallback'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
+  configureServer({ middlewares }) {
+    middlewares.use(history())
+  }
 })
